e1903e099da04e5c5fdaf9ba5fccdd07abe10a43
Sumbul Naz <164656538+sumbulNaz8@users.noreply.github.com>
2025-11-29 23:59:42 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,15 @@
 
-
-// import React from 'react' import ProductList from './Components/page' 
 import React from 'react';
 import ProductList from './Components/page';
+import Home from './Components/Home';
+import ProductDetails from './Components/ProductDetails';
 
-export default function Page ()
-{ return ( <div> { < Home /> } { <ProductDetails /> }
-  <ProductList /> 
-</div> ) }
-
-// import React from 'react';
-// import { ProductList } from './Components/page';
-
-// export default function Page() {
-//   return (
-//     <div>
-//       <ProductList />
-//     </div>
-//   );
-// }
+export default function Page() {
+  return (
+    <div>
+      <Home />
+      <ProductDetails />
+      <ProductList />
+    </div>
+  );
+}
