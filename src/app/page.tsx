e516434fb499e8f00974d75995798e8c61bcc6d8
Sumbul Naz <164656538+sumbulNaz8@users.noreply.github.com>
2025-11-29 23:48:18 +0500

    Refactor Page component for improved formatting
    Formatted the Page component for better readability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 
 
-import React from 'react' import ProductList from './Components/page' export default function Page () { return ( <div> {/* < Home /> */} {/* <ProductDetails /> */} <ProductList /> </div> ) }
+// import React from 'react' import ProductList from './Components/page' 
+import React from 'react';
+import ProductList from './Components/page';
+
+export default function Page ()
+{ return ( <div> { < Home /> } { <ProductDetails /> }
+  <ProductList /> 
+</div> ) }
 
 // import React from 'react';
 // import { ProductList } from './Components/page';
