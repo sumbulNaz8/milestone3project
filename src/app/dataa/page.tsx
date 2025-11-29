43dad092b0e72b9923785ca7b90e4d58f6f215c1
Sumbul Naz <164656538+sumbulNaz8@users.noreply.github.com>
2025-11-29 23:37:01 +0500

    Update page.tsx

--- src/app/dataa/page.tsx
+++ src/app/dataa/page.tsx
@@ -1,102 +1,199 @@
 
-interface Products {
+// interface Products {
+//   id: number;
+//   name: string;
+//   description: string;
+//   price: number;
+//   image: string;
+// }
+// const Products[] =[ 
+//     {
+      
+//       id: 1,
+//       name: 'Alfalah Mall-Lays Classic Fantastic',
+//       description: 'Description for Product 1',
+//       price: 1900,
+//        image: '/images/image2.png'
+
+//     },
+//     {
+             
+//       id: 2,
+//       name: 'Alfalah Mall-Beauty AirPods ',
+//       description: 'Description for Product 2',
+//       price: 800,
+//        image: '/images/image3.png'
+         
+//           },
+//           {
+//             id: 3,
+//             name: 'Alfalah Mall-Jack Microphone',
+//             description: 'Description for Product 3',
+//             price: 900,
+//              image: '/images/image4.png'
+       
+       
+//         }, {
+//           id: 4,
+//           name: 'Alfalah Mall-Altra Sound Stand-By Time',
+//           description: 'Description for Product 4',
+//           price: 1300,
+//            image: '/images/image6.png'
+     
+     
+//       },{
+//         id: 5,
+//         name: 'Alfalah Mall-Mobile Stand',
+//         description: 'Description for Product 5',
+//         price: 1000,
+//          image: '/images/image7.png'
+   
+   
+//     },{
+//       id: 6,
+//         name: 'Alfalah Mall-Suitable Jacet For Winter',
+//         description: 'Description for Product 6',
+//         price: 2200,
+//          image: '/images/image8.png'
+//  },{
+//   id: 7,
+//     name: 'Alfalah Mall-Caps And Glufes For Winter',
+//     description: 'Description for Product 7',
+//     price: 2300,
+//      image: '/images/image9.png'
+// },
+// {
+//   id: 8,
+//     name: 'Alfalah Mall-Suprime Nail Polish ',
+//     description: 'Description for Product 8',
+//     price: 2500,
+//      image: '/images/image10.png'
+// },
+// {
+//   id: 9,
+//     name: 'Alfalah Mall-Beautiful HandBeg',
+//     description: 'Description for Product 9',
+//     price: 1700,
+//      image: '/images/image11.png'
+// },
+// {
+//   id: 10,
+//     name: 'Alfalah Mall-Beautiful Jewellery ',
+//     description: 'Description for Product 10',
+//     price: 1600,
+//      image: '/images/image12.png'
+// },{
+//   id: 11,
+//     name: 'Alfalah Mall-Classic Chipistic',
+//     description: 'Description for Product 11',
+//     price: 1700,
+//      image: '/images/image13.png'
+// },
+// {
+//   id: 12,
+//     name: 'Alfalah Mall-Handsome Mobile Cover',
+//     description: 'Description for Product 12',
+//     price: 2900,
+//      image: '/images/image14.png'
+// },
+// ]
+
+
+// export default Products;
+
+interface Product {
   id: number;
   name: string;
   description: string;
   price: number;
   image: string;
 }
-const Products[] =[ 
-    {
-      
-      id: 1,
-      name: 'Alfalah Mall-Lays Classic Fantastic',
-      description: 'Description for Product 1',
-      price: 1900,
-       image: '/images/image2.png'
 
-    },
-    {
-             
-      id: 2,
-      name: 'Alfalah Mall-Beauty AirPods ',
-      description: 'Description for Product 2',
-      price: 800,
-       image: '/images/image3.png'
-         
-          },
-          {
-            id: 3,
-            name: 'Alfalah Mall-Jack Microphone',
-            description: 'Description for Product 3',
-            price: 900,
-             image: '/images/image4.png'
-       
-       
-        }, {
-          id: 4,
-          name: 'Alfalah Mall-Altra Sound Stand-By Time',
-          description: 'Description for Product 4',
-          price: 1300,
-           image: '/images/image6.png'
-     
-     
-      },{
-        id: 5,
-        name: 'Alfalah Mall-Mobile Stand',
-        description: 'Description for Product 5',
-        price: 1000,
-         image: '/images/image7.png'
-   
-   
-    },{
-      id: 6,
-        name: 'Alfalah Mall-Suitable Jacet For Winter',
-        description: 'Description for Product 6',
-        price: 2200,
-         image: '/images/image8.png'
- },{
-  id: 7,
+const Products: Product[] = [
+  {
+    id: 1,
+    name: 'Alfalah Mall-Lays Classic Fantastic',
+    description: 'Description for Product 1',
+    price: 1900,
+    image: '/images/image2.png'
+  },
+  {
+    id: 2,
+    name: 'Alfalah Mall-Beauty AirPods',
+    description: 'Description for Product 2',
+    price: 800,
+    image: '/images/image3.png'
+  },
+  {
+    id: 3,
+    name: 'Alfalah Mall-Jack Microphone',
+    description: 'Description for Product 3',
+    price: 900,
+    image: '/images/image4.png'
+  },
+  {
+    id: 4,
+    name: 'Alfalah Mall-Altra Sound Stand-By Time',
+    description: 'Description for Product 4',
+    price: 1300,
+    image: '/images/image6.png'
+  },
+  {
+    id: 5,
+    name: 'Alfalah Mall-Mobile Stand',
+    description: 'Description for Product 5',
+    price: 1000,
+    image: '/images/image7.png'
+  },
+  {
+    id: 6,
+    name: 'Alfalah Mall-Suitable Jacet For Winter',
+    description: 'Description for Product 6',
+    price: 2200,
+    image: '/images/image8.png'
+  },
+  {
+    id: 7,
     name: 'Alfalah Mall-Caps And Glufes For Winter',
     description: 'Description for Product 7',
     price: 2300,
-     image: '/images/image9.png'
-},
-{
-  id: 8,
-    name: 'Alfalah Mall-Suprime Nail Polish ',
+    image: '/images/image9.png'
+  },
+  {
+    id: 8,
+    name: 'Alfalah Mall-Suprime Nail Polish',
     description: 'Description for Product 8',
     price: 2500,
-     image: '/images/image10.png'
-},
-{
-  id: 9,
+    image: '/images/image10.png'
+  },
+  {
+    id: 9,
     name: 'Alfalah Mall-Beautiful HandBeg',
     description: 'Description for Product 9',
     price: 1700,
-     image: '/images/image11.png'
-},
-{
-  id: 10,
-    name: 'Alfalah Mall-Beautiful Jewellery ',
+    image: '/images/image11.png'
+  },
+  {
+    id: 10,
+    name: 'Alfalah Mall-Beautiful Jewellery',
     description: 'Description for Product 10',
     price: 1600,
-     image: '/images/image12.png'
-},{
-  id: 11,
+    image: '/images/image12.png'
+  },
+  {
+    id: 11,
     name: 'Alfalah Mall-Classic Chipistic',
     description: 'Description for Product 11',
     price: 1700,
-     image: '/images/image13.png'
-},
-{
-  id: 12,
+    image: '/images/image13.png'
+  },
+  {
+    id: 12,
     name: 'Alfalah Mall-Handsome Mobile Cover',
     description: 'Description for Product 12',
     price: 2900,
-     image: '/images/image14.png'
-},
-]
-
+    image: '/images/image14.png'
+  },
+];
 
 export default Products;
