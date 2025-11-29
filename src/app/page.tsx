b2253bf79d323212aaff0e440a2e1aff0c9c126c
Sumbul Naz <164656538+sumbulNaz8@users.noreply.github.com>
2025-11-30 00:21:59 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
 
+// import React from 'react';
+// import ProductList from './Components/page';
+// import Home from './Components/Home';
+// import ProductDetails from './Components/ProductDetails';
+
+// export default function Page() {
+//   return (
+//     <div>
+//       <Home />
+//       <ProductDetails />
+//       <ProductList />
+//     </div>
+//   );
+// }
 import React from 'react';
 import ProductList from './Components/page';
-import Home from './Components/Home';
-import ProductDetails from './Components/ProductDetails';
 
 export default function Page() {
   return (
     <div>
-      <Home />
-      <ProductDetails />
+      {/* <Home /> */}
+      {/* <ProductDetails /> */}
       <ProductList />
     </div>
   );
